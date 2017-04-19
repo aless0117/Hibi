c2f0e9085e6534e80e1eb71184304121951d6cbb
alessandro <aless0117@outlook.es>
2017-04-19 17:52:22 -0430

    Casi listo 2

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jbuilder', '~> 2.5'
   gem 'aws-sdk'
 
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
---
+++ config/initializers/translation.rb
@@ -0,0 +1,14 @@
+TranslationIO.configure do |config|
+config.api_key        = '96931ce8edf54f11945699fcbca97329'
+config.source_locale  = 'en'
+config.target_locales = ['es']
+
+# Uncomment this if you don't want to use gettext
+# config.disable_gettext = true
+
+# Uncomment this if you already use gettext or fast_gettext
+# config.locales_path = File.join('path', 'to', 'gettext_locale')
+
+# Find other useful usage information here:
+# https://github.com/aurels/translation-gem/blob/master/README.md
+end
