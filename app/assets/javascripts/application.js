4d58aa703ae3a140f1ddbf8c9a3f3595608f4b17
alessandro <aless0117@outlook.es>
2017-04-24 17:20:41 -0430

    vreo quesdfbrsd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,16 @@
 
 
 $.material.init()
+
+  $(document).on "page:load", ->
+    $('#notification-link').click ->
+      $('#notifications-form').fadeToggle()
+      $('#notification_description').focus()
+
+  $(document).on "ajax:success", "form#comments-form",(ev,data) ->
+     console.log data
+     $(this).find('textarea').val("")
+     $("#ntoficacion").append("<li> #{data.description}</li>");
+
+  $(document).on "ajax:error", "form#comments-form",(ev,data) ->
+     console.log data
